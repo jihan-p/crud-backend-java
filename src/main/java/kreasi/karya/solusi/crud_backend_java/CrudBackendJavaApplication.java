3b4a3396a63ccbd38507fe87416a4c9e138751e1
jihan-p <jihan.pahlevi@smkn-2sbg.sch.id>
2025-11-10 14:31:15 +0700

    Initial commit of Java backend structure

---
+++ src/main/java/kreasi/karya/solusi/crud_backend_java/CrudBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package kreasi.karya.solusi.crud_backend_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBackendJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kreasi/karya/solusi/crud_backend_java/CrudBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package kreasi.karya.solusi.crud_backend_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
